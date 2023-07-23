1930d6a5e07ffb4bdde17796bb184d29346f3c1a
themrsung <themrsung@gmail.com>
2023-07-23 23:57:56 +0900

    Viewport

---
+++ src/main/java/oasis/artemis/ui/component/viewport/Viewport.java
@@ -0,0 +1,11 @@
+package oasis.artemis.ui.component.viewport;
+
+import oasis.artemis.ui.component.UIComponent;
+
+/**
+ * <h2>Viewport</h2>
+ * <p>A viewport renders a level to a 2D screen.</p>
+ */
+public class Viewport extends UIComponent {
+    // HELP
+}
