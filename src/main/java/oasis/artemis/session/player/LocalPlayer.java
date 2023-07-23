653b786fce135b280ca4164c02b0062b3afa57dc
themrsung <themrsung@gmail.com>
2023-07-24 03:28:07 +0900

    Player classes

---
+++ src/main/java/oasis/artemis/session/player/LocalPlayer.java
@@ -0,0 +1,23 @@
+package oasis.artemis.session.player;
+
+import oasis.artemis.object.ArtemisObject;
+
+import javax.annotation.Nonnull;
+import java.util.UUID;
+
+/**
+ * <h2>LocalPlayer</h2>
+ * <p>A player playing locally.</p>
+ */
+public class LocalPlayer extends AbstractPlayer {
+    /**
+     * Creates a new player.
+     *
+     * @param uniqueId Unique identifier of player
+     * @param name     Name of player
+     * @param pawn     Pawn of player
+     */
+    public LocalPlayer(@Nonnull UUID uniqueId, @Nonnull String name, @Nonnull ArtemisObject pawn) {
+        super(uniqueId, name, pawn);
+    }
+}
---
+++ src/main/java/oasis/artemis/session/player/RemotePlayer.java
@@ -0,0 +1,23 @@
+package oasis.artemis.session.player;
+
+import oasis.artemis.object.ArtemisObject;
+
+import javax.annotation.Nonnull;
+import java.util.UUID;
+
+/**
+ * <h2>RemotePlayer</h2>
+ * <p>A player playing remotely.</p>
+ */
+public class RemotePlayer extends AbstractPlayer {
+    /**
+     * Creates a new player.
+     *
+     * @param uniqueId Unique identifier of player
+     * @param name     Name of player
+     * @param pawn     Pawn of player
+     */
+    public RemotePlayer(@Nonnull UUID uniqueId, @Nonnull String name, @Nonnull ArtemisObject pawn) {
+        super(uniqueId, name, pawn);
+    }
+}
